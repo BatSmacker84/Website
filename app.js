6e91a1327274fbac376ccfe10f1049f8cbdc50da
Ogden Wells <ooohhhwells@gmail.com>
2023-04-27 16:35:08 -0500

    Custom Website

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const switcher = document.querySelector('.theme-switch');
+const switcher = document.getElementById('theme-toggle');
 
 switcher.addEventListener('click', function() {
     document.body.classList.toggle('dark-theme')
@@ -13,4 +13,37 @@ switcher.addEventListener('click', function() {
     else {
         this.textContent = "Light";
     }
+});
+
+const menu = document.getElementById('menu');
+const menuItems = document.getElementsByClassName('menu-item');
+
+Array.from(menuItems)
+    .forEach((item, index) => {
+        item.onmouseover = () => {
+            menu.dataset.activeIndex = index;
+        }
+    }
+);
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// Create event listener for onmouseover for menu-item class
+// When onmouseover, set all the text of the menu-item to random letters
+Array.from(menuItems).forEach((item) => {
+    item.addEventListener('mouseover', () => {
+        let iterations = 0;
+        const interval = setInterval(() => {
+            item.textContent = item.textContent.split('')
+            .map((letter, index) => {
+                if (index < iterations) {
+                    return item.dataset.value[index];
+                }
+                return letters[Math.floor(Math.random() * 26)]
+            })
+            .join('');
+            if (iterations >= item.dataset.value.length) clearInterval(interval);
+            iterations += 1 / 3;
+        }, 30);
+    });
 });
